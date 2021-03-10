2af624b9cc9a450de7e729a251dfbb25400d4faf
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-10 08:56:06 -0500

    Created AppleAppRequest controller and injected AppleAppRequest service into it.

---
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Controllers/AppleAppRequestController.cs
@@ -0,0 +1,20 @@
+﻿using campus_technology_server.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campus_technology_server.Controllers
+{
+    public class AppleAppRequestController : Controller
+    {
+        private readonly IAppleAppRequestService appleAppRequestService;
+
+        public AppleAppRequestController(IAppleAppRequestService appleAppRequestService)
+        {
+            this.appleAppRequestService = appleAppRequestService;
+        }
+    }
+}
