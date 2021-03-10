f0f9c6d8fc8b1205f08d438e791388be54676296
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-10 08:54:32 -0500

    Added AppleAppRequest service

---
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Services/IAppleAppRequestService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campus_technology_server.Services
+{
+    public interface IAppleAppRequestService
+    {
+        
+    }
+}
