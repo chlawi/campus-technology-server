1af54a393cb0dcfc1bca72a048e8bcd73b635659
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-11 10:03:58 -0500

    Added abstract base classes

---
+++ campus-technology-server/campus-technology-server/Shared/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campus_technology_server.Shared
+{
+    abstract public class Entity
+    {
+        public int Id { get; set; }
+        public string ReferenceId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ campus-technology-server/campus-technology-server/Shared/Request.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campus_technology_server.Shared
+{
+    abstract public class Request : Entity
+    {        public DateTime Date { get; set; }
+        public string Requester { get; set; }
+        public string Approver { get; set; }
+        public StatusType Status { get; set; }
+        public string? Provider { get; set; }
+    }
+
+    public enum StatusType
+    {
+        Requested,
+        Approved,
+        Assigned,
+        Completed
+    }
+}
