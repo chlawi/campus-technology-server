6c6eabbc0ab4a92a34322d9c855f615bf488b5ea
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-16 14:36:52 -0400

    Initialize Entity IsActive field to true

--- campus-technology-server/campus-technology-server/Shared/Entity.cs
+++ campus-technology-server/campus-technology-server/Shared/Entity.cs
@@ -3,6 +3,6 @@
     abstract public class Entity
     {
         public string ReferenceId { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
