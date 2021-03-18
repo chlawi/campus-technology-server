ae7f237d35c5bbb9e3451b5e8d2ddcee0d4217a9
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-18 15:21:36 -0400

    Regenerated and refactored AppleApp request controller

--- campus-technology-server/campus-technology-server/AppleAppRequest/Controllers/AppleAppRequestController.cs
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Controllers/AppleAppRequestController.cs
@@ -1,53 +1,132 @@
 ﻿using AppleAppRequest.Models;
-using AppleAppRequest.Services;
-using Microsoft.AspNetCore.JsonPatch;
+using AutoMapper;
+using campus_technology_server.AppleAppRequest;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace AppleAppRequest.Controllers
+namespace my_playground_project.Controllers
 {
     [Route("v1/apple-app-requests")]
     [ApiController]
     public class AppleAppRequestController : ControllerBase
     {
-        private readonly IAppleAppRequestService appleAppRequestService;
+        private readonly AppleAppRequestContext _context;
 
-        public AppleAppRequestController(IAppleAppRequestService appleAppRequestService)
+        public IMapper Mapper { get; }
+
+        public AppleAppRequestController(AppleAppRequestContext context, IMapper mapper)
         {
-            this.appleAppRequestService = appleAppRequestService;
+            _context = context;
+            Mapper = mapper;
         }
 
-        public IActionResult Get()
+        // GET: api/AppleAppRequest
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppleAppRequestModel>>> GetAppleAppRequests()
         {
-            var response = new List<AppleAppRequestListView>();
-            return Ok(response);
+            return await _context.AppleAppRequests.Where(request => request.IsActive == true).ToListAsync();
         }
 
-        public IActionResult Get(int Id)
+        // GET: api/AppleAppRequest/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppleAppRequestModel>> GetAppleAppRequestModel(long id)
         {
-            var response = new AppleAppRequestDetailView();
-            return Ok(response);
+            var appleAppRequestModel = await _context.AppleAppRequests.FindAsync(id);
+
+            if (appleAppRequestModel == null || appleAppRequestModel.IsActive == false)
+            {
+                return NotFound();
+            }
+
+            return appleAppRequestModel;
         }
 
-        public IActionResult Post([FromBody] AppleAppRequestEditView request)
+        // PUT: api/AppleAppRequest/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppleAppRequestModel(long id, AppleAppRequestModel appleAppRequestModel)
         {
-            var response = new AppleAppRequestEditView();
-            return Ok(response);
+            if (id != appleAppRequestModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appleAppRequestModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppleAppRequestModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        public IActionResult Put(int Id, [FromBody] AppleAppRequestEditView request)
+        // POST: api/AppleAppRequest
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AppleAppRequestModel>> PostAppleAppRequestModel(AppleAppRequestEditView appleAppRequestView)
         {
-            return Ok();
+            var appleAppRequest = Mapper.Map<AppleAppRequestModel>(appleAppRequestView);
+
+            appleAppRequestView.RequestedApplications.ForEach(requestedApplication =>
+            {
+                if (!_context.AppleAppApplications.Any(a => a.Name.ToUpper() == requestedApplication.Name.ToUpper()))
+                {
+                    var newAppleAppApplication = new AppleAppApplicationModel { Name = requestedApplication.Name, ReferenceId = Guid.NewGuid().ToString() };
+                    _context.AppleAppApplications.Add(newAppleAppApplication);
+                }
+            });
+
+            appleAppRequestView.RequestedDevices.ForEach(requestedDevice =>
+            {
+                if (!_context.AppleAppDevices.Any(d => d.AssetTag.ToUpper() == requestedDevice.AssetTag.ToUpper()))
+                {
+                    var newAppleAppDevice = new AppleAppDeviceModel { AssetTag = requestedDevice.AssetTag, ReferenceId = Guid.NewGuid().ToString() };
+                    _context.AppleAppDevices.Add(newAppleAppDevice);
+                }
+            });
+
+            _context.AppleAppRequests.Add(appleAppRequest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAppleAppRequestModel", new { id = appleAppRequest.Id }, appleAppRequest);
         }
 
-        public IActionResult Patch(int Id, [FromBody] JsonPatchDocument<AppleAppRequestEditView> request)
+        // DELETE: api/AppleAppRequest/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppleAppRequestModel(long id)
         {
-            return Ok();
+            var appleAppRequestModel = await _context.AppleAppRequests.FindAsync(id);
+            if (appleAppRequestModel == null)
+            {
+                return NotFound();
+            }
+
+            appleAppRequestModel.IsActive = false;
+            _context.AppleAppRequests.Update(appleAppRequestModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
-        public IActionResult Delete(int Id)
+        private bool AppleAppRequestModelExists(long id)
         {
-            return Ok();
+            return _context.AppleAppRequests.Any(e => e.Id == id);
         }
     }
 }
