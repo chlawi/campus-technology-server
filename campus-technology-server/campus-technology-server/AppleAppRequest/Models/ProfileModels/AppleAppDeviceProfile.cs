fa97934bc2aecb87454eca089f6de7b1cf8f5c78
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-19 07:46:23 -0400

    Added AppleAppDevice automapping profile

---
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Models/ProfileModels/AppleAppDeviceProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleAppRequest.Models
+{
+    public class AppleAppDeviceProfile : Profile
+    {
+        public AppleAppDeviceProfile()
+        {
+            CreateMap<AppleAppRequestedDeviceEditView, AppleAppRequestedDeviceModel>()
+                .ReverseMap();
+        }
+    }
+}
