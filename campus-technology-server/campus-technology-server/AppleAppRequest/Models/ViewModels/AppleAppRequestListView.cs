2be0cf71a0007ceea335e21d235c49da84956603
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-19 12:24:26 -0400

    Implemented AppleAppRequest list view model

--- campus-technology-server/campus-technology-server/AppleAppRequest/Models/ViewModels/AppleAppRequestListView.cs
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Models/ViewModels/AppleAppRequestListView.cs
@@ -1,6 +1,15 @@
-﻿namespace AppleAppRequest.Models
+﻿using System;
+
+namespace AppleAppRequest.Models
 {
     public class AppleAppRequestListView
     {
+        public int Id { get; set; }
+        public string ReferenceId { get; set; }
+        public string RequesterId { get; set; }
+        public string ApproverId { get; set; }
+        public StatusType Status { get; set; }
+        public DateTime Date { get; set; } 
+        public SubscriptionType SubscriptionType { get; set; }
     }
 }
