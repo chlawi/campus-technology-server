fd11b1bbf372c48394d7b65b79a35ae1c688fb01
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-19 07:41:04 -0400

    Added AppleAppApplication auto mapping profile

---
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Models/ProfileModels/AppleAppApplicationProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleAppRequest.Models
+{
+    public class AppleAppApplicationProfile : Profile
+    {
+        public AppleAppApplicationProfile()
+        {
+            CreateMap<AppleAppRequestedApplicationEditView, AppleAppRequestedApplicationModel>()
+                .ReverseMap();
+        }
+    }
+}
