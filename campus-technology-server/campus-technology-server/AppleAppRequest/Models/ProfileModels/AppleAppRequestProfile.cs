823d04c4ddb3d1138404522215ac78e0331b4860
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-03-19 07:42:23 -0400

    Relocated AppleAppRequest auto mapping profile

--- campus-technology-server/campus-technology-server/AppleAppRequest/Models/ProfileModels/AppleAppRequestProfile.cs
+++ campus-technology-server/campus-technology-server/AppleAppRequest/Models/ProfileModels/AppleAppRequestProfile.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace campus_technology_server.AppleAppRequest.Models
+namespace AppleAppRequest.Models
 {
     public class AppleAppRequestProfile : Profile
     {
