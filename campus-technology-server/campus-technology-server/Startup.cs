91e76e912fb1050844cadf67bc21995f209b9fc6
Chet Williams <Chet_Williams@BOCES.monroe.edu>
2021-05-28 08:29:33 -0400

    added jwt token authentication

--- campus-technology-server/campus-technology-server/Startup.cs
+++ campus-technology-server/campus-technology-server/Startup.cs
@@ -1,5 +1,6 @@
 using AppleAppRequest.Services;
 using campus_technology_server.AppleAppRequest;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 
 namespace campus_technology_server
 {
@@ -39,6 +41,30 @@ namespace campus_technology_server
             //services.AddCors(options => { options.AddPolicy("AllowAllWithCredentials", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()); });
             services.AddCors(options => { options.AddPolicy("AllowAllWithCredentials", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); });
 
+            var tenantId = $"{Configuration["AzureAD:TenantId"]}";
+            var authority = $"{Configuration["AzureAD:Instance"]}/{tenantId}";
+
+            var audience = new List<string>
+            {
+                $"{Configuration["AzureAD:ClientId"]}",
+                $"{Configuration["AzureAD:Scope"]}"
+            };
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => 
+                    {
+                        options.Authority = authority;
+                        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                        {
+                            ValidAudiences = audience,
+                            ValidIssuers = new List<string>
+                            {
+                                $"https://sts.windows.net/{tenantId}",
+                                $"https://sts.windows.net/{tenantId}/v2.0"
+                            }
+                        };
+                    });
+
             services
                 .AddControllers()
                 .AddNewtonsoftJson
@@ -67,6 +93,9 @@ namespace campus_technology_server
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
